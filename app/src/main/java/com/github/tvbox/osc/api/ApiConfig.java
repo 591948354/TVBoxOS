e8e26c9ba9f9f22e94ec8fdb61691c7f86e0377c
591948354 <44075938+591948354@users.noreply.github.com>
2022-08-23 10:13:57 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -80,7 +80,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "http://maoyingshi.cc/tvbox/云星日记/ali.m3u8");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
